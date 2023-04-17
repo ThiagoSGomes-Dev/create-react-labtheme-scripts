55cb3165b9859b0a5dcb1ab190c12ede9808c166
ThiagoGs-Dev <96384024+ThiagoGs-Dev@users.noreply.github.com>
2023-04-17 20:09:15 -0300

    lab test-scripts-v3

--- packages/react-scripts-labtheme/scripts/init-labtheme.js
+++ packages/react-scripts-labtheme/scripts/init-labtheme.js
@@ -1,6 +1,6 @@
 // @remove-file-on-eject
 /**
- * Copyright (c) 2019-present, https://github.com/devloco
+ * Copyright (c) 2023-present, https://github.com/ThiagoSGomes-Dev
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
